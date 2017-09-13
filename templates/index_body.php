10c1a9224f608e65cae3cee74c62c36224a90b98
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-09-14 00:55:43 +0300

    Implemented most of the desing that will go into the 1st page.

--- templates/index_body.php
+++ templates/index_body.php
@@ -1,3 +1,127 @@
 <?php 
 
     Page::_loadTemplate("navbar");
+
+?>
+
+<div class="spacer2x"></div>
+
+<div class="container-fluid main-container center bg-dark">
+    <h1 class="display-4">Kodėl verta pirkti mūsų produkciją?</h1>
+</div>
+<div class="container-fluid main-container bg-dark2">
+    <div class="media left">
+        <i class="fa fa-wrench fa-5x d-flex"></i>
+            
+        <div class="media-body">
+            <h4 class="mt-0">Blah blah blah blah blah blah</h4>
+            <font>
+                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+            </font>
+        </div>
+    </div>
+</div>
+<div class="container-fluid main-container bg-dark">
+    <div class="media right">
+    <div class="media-body">
+        <h4 class="mt-0">Blah blah blah blah blah blah</h4>
+        <font>
+            Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+            Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+        </font>
+    </div>
+
+        <i class="fa fa-wrench fa-5x d-flex"></i>
+    </div>
+</div>
+<div class="container-fluid main-container bg-dark2">
+    <div class="media left">
+        <i class="fa fa-wrench fa-5x d-flex"></i>
+            
+        <div class="media-body">
+            <h4 class="mt-0">Blah blah blah blah blah blah</h4>
+            <font>
+                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+            </font>
+        </div>
+    </div>
+</div>
+<div class="container-fluid end-container bg-dark"></div>
+
+<div class="spacer"></div>
+
+<div class="container-fluid main-container center bg-dark">
+    <h1 class="display-4">Items4You produkcija:</h1>
+</div>
+
+<div class="container-fluid main-container center bg-dark2" id="products-container">
+    
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap">
+
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap">
+
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap">
+
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap">
+
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap">
+
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card-img-top" src="..." alt="Card image cap">
+
+        <div class="card-block">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+</div>
+
+<div class="container-fluid end-container bg-dark"></div>
+
+<div class="spacer"></div>
+
+<div class="container-fluid center bg-dark">Copyright notice</div>
