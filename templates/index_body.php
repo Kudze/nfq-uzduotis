4b8a799de8e56428e0fadb1c317b5afe1c94fe0b
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-09-15 21:50:42 +0300

    Ah ffs so I was meant to make one product and 50 orders for this page instead of 50 products and 50 orders. That means a lot of refactoring and removing code :I
    *Wrote all margeting texts for webpage.
    *Removed Product system (Atleast from the front-end, since i can reuse some stuff for orders system).

--- templates/index_body.php
+++ templates/index_body.php
@@ -7,17 +7,17 @@
 <div class="spacer2x"></div>
 
 <div class="container-fluid main-container center bg-dark">
-    <h1 class="display-4">Kodėl verta pirkti mūsų produkciją?</h1>
+    <h1 class="display-4">Kodėl verta pirkti Print3D Spausdintuvą?</h1>
 </div>
 <div class="container-fluid main-container bg-dark2">
     <div class="media left">
-        <i class="fa fa-wrench fa-5x d-flex"></i>
+        <i class="fa fa-clock-o fa-fw fa-5x d-flex"></i>
             
         <div class="media-body">
-            <h4 class="mt-0">Blah blah blah blah blah blah</h4>
+            <h4 class="mt-0">Dvigubai trumpesnis spausdinimo laikas, nei konkurentų spausdintuvuose!</h4>
             <font>
-                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
-                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+                Print3D spausdintuvai naudoja naują Items4You&trade; materiją, kuri pilnai tinka spausdinimui, bei greičiau stingsta, nei mūsų konkurentų naudojamos medžiagos. 
+                Būtent todėl mūsų spausdintuvai yra dvigubai greitesni!
             </font>
         </div>
     </div>
@@ -25,25 +25,24 @@
 <div class="container-fluid main-container bg-dark">
     <div class="media right">
     <div class="media-body">
-        <h4 class="mt-0">Blah blah blah blah blah blah</h4>
+        <h4 class="mt-0">Žymiai pigesnis produktas</h4>
         <font>
-            Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
-            Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+            Kadangi Items4You&trade; materija labiau tinka 3D spausdinimui negu bet kokia kita materija, galėjome iš standartinio 3D spausdintuvo panaikinti kelis komponentus, 
+            taip sumažindami gaminimo kaštus.
         </font>
     </div>
 
-        <i class="fa fa-wrench fa-5x d-flex"></i>
+        <i class="fa fa-money fa-5x fa-fw d-flex"></i>
     </div>
 </div>
 <div class="container-fluid main-container bg-dark2">
     <div class="media left">
-        <i class="fa fa-wrench fa-5x d-flex"></i>
+        <i class="fa fa-wrench fa-5x fa-fw d-flex"></i>
             
         <div class="media-body">
-            <h4 class="mt-0">Blah blah blah blah blah blah</h4>
+            <h4 class="mt-0">5 Metų garantija</h4>
             <font>
-                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
-                Blah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blahBlah blah blah blah blah blah.
+                Kadangi mums yra svarbiausia yra tobulinti 3D spausdinimo sferą, ir auginti žmonių pasitikėjimą mumis. Mes savo produkcijai skiriame 5 metų garantiją.
             </font>
         </div>
     </div>
@@ -53,12 +52,18 @@
 <div class="spacer"></div>
 
 <div class="container-fluid main-container center bg-dark">
-    <h1 class="display-4">Items4You produkcija:</h1>
+    <h1 class="display-4">Kaip užsisakyti Print3D spausdintuvą?</h1>
 </div>
 
 <div class="container-fluid main-container center bg-dark2" id="products-container">
 
-    <?php ProductManager::renderProductsList(); ?>
+    <h4>Norint užsisakyti 3D spausdintuvą jums reikia užpildyti šią formą, o vėliau mes patys su jumis susisieksime!</h3>
+
+    <form action="index.php?page=orders" method="post">
+
+    
+
+    </form>
 
 </div>
 
