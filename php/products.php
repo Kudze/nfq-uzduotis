39603858d184fd4210edc1038272061d0ea7b308
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-09-15 21:19:11 +0300

    Implemented Back-End for products search list.

--- php/products.php
+++ php/products.php
@@ -6,23 +6,27 @@ class ProductManager {
 
         $currPage = @$_GET["pPage"]; 
         $maxPage;
-        $products = self::_fetchProductsList(12, $currPage, $maxPage); //12 is good because it does divide with 1, 2, 3, 4 and 6.
+        $search = @$_GET["pSearch"];
+        $products = self::_fetchProductsList(12, $currPage, $maxPage, $search); //12 is good because it does divide with 1, 2, 3, 4 and 6.
 
         Page::_loadTemplate("product");
-        _renderProductsTop(1, $currPage, $maxPage);
+        _renderProductsTop(1, $currPage, $maxPage, $search);
 
         foreach($products as $product)
             $product->render();
 
     }
 
-    private static function _fetchProductsList($itemsPerPage, &$currPage, &$maxPage) {
+    private static function _fetchProductsList($itemsPerPage, &$currPage, &$maxPage, $search) {
         $res = array();
 
         try {
+            
+            $realSearch = "%" . $search . "%";
 
             //We get product count from database
-            $stmt = Database::getConnection()->prepare("SELECT COUNT(*) AS 'count' FROM `products` LIMIT 1");
+            $stmt = Database::getConnection()->prepare("SELECT COUNT(*) AS 'count' FROM `products` WHERE `name` LIKE :search LIMIT 1");
+            $stmt->bindParam(":search", $realSearch); //Afaik this does protect me against sql injection.
             $stmt->execute();
             $itemCount = $stmt->fetch()['count'];
 
@@ -31,7 +35,8 @@ class ProductManager {
             if(!is_numeric($currPage) || $currPage > $maxPage) $currPage = 1; //This should stop any SQL Injection or messing with get values issues.
 
             //Then we fetch data for the page that we need to show.
-            $stmt = Database::getConnection()->prepare("SELECT * FROM `products` LIMIT :offset, :count");
+            $stmt = Database::getConnection()->prepare("SELECT * FROM `products` WHERE `name` LIKE :search LIMIT :offset, :count");
+            $stmt->bindParam(":search", $realSearch); //Afaik this does protect me against sql injection.
             $stmt->bindValue(":offset", ($currPage - 1) * $itemsPerPage, PDO::PARAM_INT);
             $stmt->bindValue(":count", $itemsPerPage, PDO::PARAM_INT);
             $stmt->execute();
--- templates/product.php
+++ templates/product.php
@@ -24,51 +24,61 @@
 
     }
 
-    function _renderProductsTop($minPage, $currPage, $maxPage) {
+    function _renderProductsTop($minPage, $currPage, $maxPage, $searchText) {
 
         //Start of top block
         echo '<center>';
 
         //Search Form
         echo '
-            <form class="inline-form search-form">
-                <input type="text" class="form-control" id="pSearchText" name="pSearchText" placeholder="Paieška">
+            <form class="inline-form search-form' . ($minPage <= $maxPage ? ' search-form-wpg' : '') . '" action="index.php#products-container" method="get">
+                <input type="hidden" id="page" name="page" value="index">
+                <input type="hidden" id="pPage" name="pPage" value="1">
+                <input type="text" class="form-control" id="pSearch" name="pSearch" placeholder="Paieška" value="' . $searchText . '">
                 <button type="submit" class="btn btn-outline-success" id="pSearchSubmit" name="pSearchSubmit"><i class="fa fa-search" aria-hidden="true"></i></button>
             </form>
         ';
 
-        //Start of pagination.
-        echo '<ul class="pagination">';
+        //We dont want any pages when there's no items at all.
+        if($minPage <= $maxPage) {
+            //Start of pagination.
+            echo '<ul class="pagination">';
 
-        //Pagination content
-        _renderPageControl(1, $currPage != $minPage ? $currPage - 1 : $maxPage);
-              
-        for($i = $minPage; $i <= $maxPage; $i++)
-            _renderPage($i, $i == $currPage);
+            //Pagination content
+            _renderPageControl(1, $currPage != $minPage ? $currPage - 1 : $maxPage, $searchText);
+                
+            for($i = $minPage; $i <= $maxPage; $i++)
+                _renderPage($i, $i == $currPage, $searchText);
 
-        _renderPageControl(0, $currPage != $maxPage ? $currPage + 1 : $minPage);
+            _renderPageControl(0, $currPage != $maxPage ? $currPage + 1 : $minPage, $searchText);
 
-        //End of pagination
-        echo '</ul>';
+            //End of pagination
+            echo '</ul>';
 
-        //End of top block.
-        echo '</center>';
+            //End of top block.
+            echo '</center>';
+        }
+
+        else
+            echo '
+            <center class="alert alert-danger">Nėra jokių produktų tokiu pavadinimu!</center>
+            ';
 
     }
 
-    function _renderPage($pageNum, $active) {
+    function _renderPage($pageNum, $active, $searchText) {
         if($active)
             echo '<li class="page-item"><span class="page-link active">' . $pageNum . '</span></li>';
         else
-            echo '<li class="page-item"><a class="page-link" href="' . Page::getCurrentURL() . '&pPage=' . $pageNum . '#products-container">' . $pageNum . '</a></li>';
+            echo '<li class="page-item"><a class="page-link" href="' . Page::getCurrentURL() . '&pPage=' . $pageNum . '&pSearch=' . $searchText . '#products-container">' . $pageNum . '</a></li>';
     }
 
     //$dir = 0 (right)
     //$dir = 1 (left)
-    function _renderPageControl($dir, $pageNum) {
+    function _renderPageControl($dir, $pageNum, $searchText) {
         echo '
         <li class="page-item">
-            <a class="page-link" href="' . Page::getCurrentURL() . '&pPage=' . $pageNum . '#products-container" aria-label="' . ($dir ? 'Praeitas' : 'Kitas') . '">
+            <a class="page-link" href="' . Page::getCurrentURL() . '&pPage=' . $pageNum . '&pSearch=' . $searchText . '#products-container" aria-label="' . ($dir ? 'Praeitas' : 'Kitas') . '">
                 <span aria-hidden="true">' . ($dir ? '&laquo;' : '&raquo;') . '</span>
                 <span class="sr-only">' . ($dir ? 'Praeitas' : 'Kitas') . '</span>
             </a>
