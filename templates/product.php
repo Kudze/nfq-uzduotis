954c253fd1fd28ecf381a50feeafac7089009179
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-09-15 14:06:05 +0300

    Added left and right buttons  from pagination to back-end.

--- templates/product.php
+++ templates/product.php
@@ -28,28 +28,17 @@
 
         echo '
             <center>
+                <ul class="pagination">
         ';
 
-        echo '
-                <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Praeitas">
-                    <span aria-hidden="true">&laquo;</span>
-                    <span class="sr-only">Praeitas</span>
-                    </a>
-                </li>
-        ';
+        _renderPageControl(1, $currPage != $minPage ? $currPage - 1 : $maxPage);
               
         for($i = $minPage; $i <= $maxPage; $i++)
             _renderPage($i, $i == $currPage);
 
+        _renderPageControl(0, $currPage != $maxPage ? $currPage + 1 : $minPage);
+
         echo '
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Kitas">
-                    <span aria-hidden="true">&raquo;</span>
-                    <span class="sr-only">Kitas</span>
-                    </a>
-                </li>
                 </ul>
             </center>
         ';
@@ -61,6 +50,19 @@
             echo '<li class="page-item"><span class="page-link active">' . $pageNum . '</span></li>';
         else
             echo '<li class="page-item"><a class="page-link" href="' . Page::getCurrentURL() . '&pPage=' . $pageNum . '#products-container">' . $pageNum . '</a></li>';
+    }
+
+    //$dir = 0 (right)
+    //$dir = 1 (left)
+    function _renderPageControl($dir, $pageNum) {
+        echo '
+        <li class="page-item">
+            <a class="page-link" href="' . Page::getCurrentURL() . '&pPage=' . $pageNum . '#products-container" aria-label="' . ($dir ? 'Praeitas' : 'Kitas') . '">
+                <span aria-hidden="true">' . ($dir ? '&laquo;' : '&raquo;') . '</span>
+                <span class="sr-only">' . ($dir ? 'Praeitas' : 'Kitas') . '</span>
+            </a>
+        </li>
+        ';
     }
 
 ?>
